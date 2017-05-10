210a61caa2e5e5425e0aa6afc11db6f648f4704e
vkrishnajayanthi <vkrishnajayanthi>
2017-05-09 20:52:29 -0700

    Added DocController as the main abstract class for all controllers

---
+++ dynamicdocrest/src/main/java/dyndoc/util/Constants.java
@@ -0,0 +1,13 @@
+package dyndoc.util;
+
+/**
+ * Created by krish on 5/9/2017.
+ * This is a simple Constants file that holds all the constants we'd be using in this app.
+ */
+public class Constants {
+
+    /**
+     * The version number for our API. Currently set to v1.
+     */
+    public static final String VERSION = "/v1";
+}
